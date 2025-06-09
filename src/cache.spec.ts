d09f974fbfe6e4e05a92dcff86eea4b909140b9a
Mark <hendriksen-mark@hotmail.com>
2025-06-09 03:31:21 +0200

    Update cache.spec.ts

--- src/cache.spec.ts
+++ src/cache.spec.ts
@@ -1,4 +1,4 @@
-import {Cache} from './cache';
+import { Cache } from './cache';
 
 let cache: Cache;
 
@@ -10,6 +10,7 @@ describe('Cache', () => {
   it('should return shouldQuery=true on first call', () => {
     expect(cache.shouldQuery()).toBe(true);
   });
+
   it('should return shouldQuery=false shortly after last query', () => {
     cache.queried();
     expect(cache.shouldQuery()).toBe(false);
@@ -30,5 +31,37 @@ describe('Cache', () => {
     cache.queried();
     expect(cache.shouldQuery()).toBe(false);
     expect(cache.isInfinite()).toBe(true);
+  });
+
+  it('should use defaultTime if cacheTime is undefined', () => {
+    const cache = new Cache(undefined, 10);
+    expect(cache.cacheTime).toBe(10);
+  });
+
+  it('should update lastQueried when queried() is called', () => {
+    const before = cache.lastQueried;
+    cache.queried();
+    expect(cache.lastQueried).toBeGreaterThanOrEqual(before);
+  });
+
+  it('should return shouldQuery=true if cacheTime is 0', () => {
+    const cache = new Cache(0);
+    expect(cache.shouldQuery()).toBe(true); // always true if cacheTime is 0
+    cache.queried();
+    expect(cache.shouldQuery()).toBe(true); // still true after queried
+  });
+
+  it('should return shouldQuery=false immediately after queried() with positive cacheTime', () => {
+    cache.queried();
+    expect(cache.shouldQuery()).toBe(false);
+  });
+
+  it('should return shouldQuery=true after enough time has passed', (done) => {
+    const cache = new Cache(5);
+    cache.queried();
+    setTimeout(() => {
+      expect(cache.shouldQuery()).toBe(true);
+      done();
+    }, 10);
   });
 });
